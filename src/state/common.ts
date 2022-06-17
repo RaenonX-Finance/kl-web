9bdcd1b17b844e29e1c7e5f23dac7a3cfaad0ba4
RaenonX <raenonx0710@gmail.com>
2022-06-16 23:42:37 -0500

    Fixed custom SR not working #14
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/state/common.ts
+++ src/state/common.ts
@@ -1,9 +1,11 @@
 export const wholeStateUpdateReducer = <T>(
   onEachValue?: (value: T) => T,
-) => <S extends Record<number, T>, P extends Record<number, T>>(
+) => <S extends Record<string, T>, P extends Record<string, T>>(
   state: S, {payload}: {payload: P},
 ) => {
   // Remove all then add it back
-  Object.keys(state).forEach((key) => delete state[parseInt(key)]);
-  Object.entries(payload).forEach(([key, value]) => state[parseInt(key)] = onEachValue ? onEachValue(value) : value);
+  Object.keys(state).forEach((key) => delete state[key]);
+  // False positive of `key` not being able to index on state
+  // @ts-ignore
+  Object.entries(payload).forEach(([key, value]) => state[key] = onEachValue ? onEachValue(value) : value);
 };
