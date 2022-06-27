ea75f7f8704f9f9eac1b96fdf5095eab32fd2cc7
RaenonX <raenonx0710@gmail.com>
2022-06-26 19:43:01 -0500

    Optimized loading icon showing
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/chart/layout/pxData.tsx
+++ src/components/chart/layout/pxData.tsx
@@ -18,21 +18,36 @@ type Props = {
 export const PxDataLayoutPane = ({pxData, width, height, x, y}: Props) => {
   const customSrLevels = useCustomSrSelector();
 
-  if (pxData === null) {
-    return <Loading/>;
-  }
+  const containerCss: React.CSSProperties = {
+    position: 'absolute',
+    // Set the container size for Px data chart
+    left: `${x}px`,
+    top: `${y}px`,
+    width: `${width}px`,
+    height: `${height}px`,
+    // Mainly for centering the loading icon
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
 
   return (
-    <div style={{position: 'absolute', left: `${x}px`, top: `${y}px`}}>
-      <PxDataChart
-        title={getPxDataTitle(pxData)}
-        chartData={pxData}
-        payload={{
-          customSrLevels: customSrLevels[pxData.contract.symbol],
-        }}
-        height={height}
-        width={width}
-      />
+    <div style={containerCss}>
+      {
+        pxData === null ?
+          <Loading/> :
+          <React.Suspense fallback={<Loading/>}>
+            <PxDataChart
+              title={getPxDataTitle(pxData)}
+              chartData={pxData}
+              payload={{
+                customSrLevels: customSrLevels[pxData.contract.symbol],
+              }}
+              height={height}
+              width={width}
+            />
+          </React.Suspense>
+      }
     </div>
   );
 };
