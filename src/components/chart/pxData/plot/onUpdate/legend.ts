4fb7890c137a124941a2c032c0b9ac2cdb562fee
RaenonX <raenonx0710@gmail.com>
2022-06-28 02:00:32 -0500

    Fixed erroneous strength on neutral
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/chart/pxData/plot/onUpdate/legend.ts
+++ src/components/chart/pxData/plot/onUpdate/legend.ts
@@ -7,6 +7,6 @@ export const handleLegend = ({chartDataRef, setObject}: OnPxChartUpdatedEvent) =
     // Only update the legend on Px changed if not hovered,
     // So even if the latest bar is updated, the legend won't change
     ...(legend.hovered ? {} : chartDataRef.current.latestMarket),
-    strength: chartDataRef.current.data.at(-1)?.strength || '?',
+    strength: chartDataRef.current.data.at(-1)?.strength ?? '?',
   }));
 };
