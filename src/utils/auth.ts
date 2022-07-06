a26d3563d2dd0b2de40a964f70a917649a174b5d
RaenonX <raenonx0710@gmail.com>
2022-07-06 00:30:10 -0500

    Added token redirect URI validation RaenonX-Finance/kl-site-back#38
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -11,18 +11,19 @@ type RequestOAuth2TokenData = {
 export const requestOAuth2Token = (
   username: string,
   password: string,
-): Promise<AxiosResponse<RequestOAuth2TokenResponse, RequestOAuth2TokenData>> => (
-  axios.request({
+): Promise<AxiosResponse<RequestOAuth2TokenResponse, RequestOAuth2TokenData>> => {
+  const data = new URLSearchParams(window.location.search);
+
+  data.set('username', username);
+  data.set('password', password);
+
+  return axios.request({
     url: process.env.NEXT_PUBLIC_AUTH_URL_TOKEN,
     method: 'POST',
     // Using `Content-Type: application/x-www-form-urlencoded`, so `data` must be `URLSearchParams`
-    data: new URLSearchParams({
-      // scope: 'oauth scopes separated with spaces',
-      username,
-      password,
-    }),
-  })
-);
+    data,
+  });
+};
 
 export const getNextAuthCallbackUrl = (accessToken: string): string => {
   const url = new URL(`${process.env.NEXT_PUBLIC_AUTH_URL}/api/auth/callback/${CUSTOM_PROVIDER_ID}`);
