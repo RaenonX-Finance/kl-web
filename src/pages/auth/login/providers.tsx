d2d1c2f54d6cf9910f2b83d87f495ebb6554c74f
RaenonX <raenonx0710@gmail.com>
2022-07-09 13:32:10 -0500

    Improved error handling on login via external provider
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/pages/auth/login/providers.tsx
+++ src/pages/auth/login/providers.tsx
@@ -5,6 +5,9 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
+import {errorDispatchers} from '../../../state/error/dispatchers';
+import {ErrorDispatcherName} from '../../../state/error/types';
+import {useDispatch} from '../../../state/store';
 import {CUSTOM_PROVIDER_ID} from '../../../types/auth/const';
 import {providerIcon} from './icons';
 import {AuthLoginPageProps} from './main';
@@ -12,6 +15,8 @@ import styles from './main.module.scss';
 
 
 export const AuthLoginProviders = ({providers}: AuthLoginPageProps) => {
+  const dispatch = useDispatch();
+
   if (!providers) {
     return <></>;
   }
@@ -26,7 +31,10 @@ export const AuthLoginProviders = ({providers}: AuthLoginPageProps) => {
             .map((provider) => (
               <Button
                 key={provider.name} variant="outline-light" className={styles['provider-button']}
-                onClick={() => signIn(provider.id)}
+                onClick={() => signIn(provider.id).catch((error) => {
+                  console.error(error);
+                  dispatch(errorDispatchers[ErrorDispatcherName.UPDATE]({message: JSON.stringify(error)}));
+                })}
               >
                 {providerIcon[provider.name] || provider.name}
               </Button>
