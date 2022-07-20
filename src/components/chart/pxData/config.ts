43eb9dbab529211b9542bf362090d90ec403026f
RaenonX <raenonx0710@gmail.com>
2022-07-19 21:29:39 -0500

    Updated min config intervals
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/chart/pxData/config.ts
+++ src/components/chart/pxData/config.ts
@@ -68,14 +68,14 @@ export const configEntriesUI: PxChartLayoutConfigUI = {
     title: '行情刷新頻率 (秒)',
     group: '其他',
     step: 0.05,
-    min: 0.05,
+    min: 0.1,
     tips: '秒數越低，系統資源消耗量越大；報價更新頻率越快。',
   },
   intervalHistoryPxSec: {
     title: '歷史資料要求頻率 (秒)',
     group: '其他',
     step: 1,
-    min: 1,
+    min: 3,
     tips: '秒數越低，網路流量消耗越大；資料準確度更高。',
   },
 };
