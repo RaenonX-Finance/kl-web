6c5391844583f23050172a73505f01bd84f7e8e5
RaenonX <raenonx0710@gmail.com>
2022-08-03 22:59:22 -0500

    Updated expiry check to accept ISO string
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/pages/admin/tabs/accounts/utils.ts
+++ src/pages/admin/tabs/accounts/utils.ts
@@ -1,8 +1,19 @@
+import {ISOTimestampWithTimezone} from '../../../../types/auth/user';
 import {Account} from './main';
 import styles from './main.module.scss';
 
 
-export const isExpired = (expiry: Date | null): boolean => expiry ? new Date() > expiry : false;
+export const isExpired = (expiry: Date | ISOTimestampWithTimezone | null): boolean => {
+  if (expiry) {
+    if (typeof expiry === 'string') {
+      return new Date() > new Date(expiry);
+    }
+
+    return new Date() > expiry;
+  }
+
+  return false;
+};
 
 export const getAccountRowClassName = ({expiry, admin, blocked}: Account): string => {
   if (blocked) {
