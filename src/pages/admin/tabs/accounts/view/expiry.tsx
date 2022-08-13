8f74b2bee122aa072b24e405d554483f2b0faf11
RaenonX <raenonx0710@gmail.com>
2022-08-13 05:26:09 -0500

    Fixed crash on invalid account expiry
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/pages/admin/tabs/accounts/view/expiry.tsx
+++ src/pages/admin/tabs/accounts/view/expiry.tsx
@@ -9,7 +9,7 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import {errorDispatchers} from '../../../../../state/error/dispatchers';
 import {ErrorDispatcherName} from '../../../../../state/error/types';
 import {useDispatch} from '../../../../../state/store';
-import {ISODateString} from '../../../../../types/time';
+import {ISODateString, ISOTimestampWithTimezone} from '../../../../../types/time';
 import {apiUpdateExpiry} from '../../../../../utils/api/admin';
 import {getErrorMessage} from '../../../../../utils/error';
 import {isAllowed} from '../../../../../utils/permission';
@@ -54,7 +54,18 @@ export const AccountExpiry = ({account}: Props) => {
         <Form.Control
           type="date"
           value={expiryString}
-          onChange={({target}) => setExpiryState(`${target.value as ISODateString}T23:59:59.999+00:00`)}
+          onChange={({target}) => {
+            const expiryString: ISOTimestampWithTimezone = `${target.value as ISODateString}T23:59:59.999+00:00`;
+
+            try {
+              // Test format as `target.value` could be an invalid date string
+              format(new Date(expiryString), 'yyyy-MM-dd');
+            } catch (e) {
+              return;
+            }
+
+            setExpiryState(expiryString);
+          }}
           // Use `key` to clear input
           // https://stackoverflow.com/a/69536313/11571888
           key={(!expiryState).toString()}
