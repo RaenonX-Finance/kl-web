fbbc771c874ebaec1b50a3a861d6ab107aec923d
RaenonX <raenonx0710@gmail.com>
2022-08-25 00:17:24 -0500

    Fixed strength not updated
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/chart/pxData/plot/onUpdate/legend.ts
+++ src/components/chart/pxData/plot/onUpdate/legend.ts
@@ -7,5 +7,7 @@ export const handleLegend = ({chartDataRef, setObject}: OnPxChartUpdatedEvent) =
     // Only update the legend on Px changed if not hovered,
     // So even if the latest bar is updated, the legend won't change
     ...(legend.hovered ? {} : chartDataRef.current.latestMarket),
+    // Always update strength regardless the legend hovering status
+    strength: chartDataRef.current.strength,
   }));
 };
