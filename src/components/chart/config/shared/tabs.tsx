410540e3ee31677b56cc9dbcae8c44d11467006b
RaenonX <raenonx0710@gmail.com>
2023-02-09 04:57:36 -0600

    Removed shared config settings panel
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/components/chart/config/shared/tabs.tsx
+++ src/components/chart/config/shared/tabs.tsx
@@ -5,14 +5,9 @@ import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
 
-import {defaultSharedConfig} from '../../../../state/config/const';
-import {getSharedConfig} from '../../../../state/config/utils';
-import {FloatingInput} from '../../../common/form/floating/input';
-import {TextWithLoading} from '../../../common/loading/text';
 import {ChartLayoutSelector} from '../../layoutSelector/main';
-import {groupedSharedConfigEntries} from '../const';
 import styles from './main.module.scss';
-import {PxSharedConfig, PxSharedConfigKeys} from './type';
+import {PxSharedConfig} from './type';
 
 
 type Props = {
@@ -22,7 +17,7 @@ type Props = {
   updating: boolean,
 };
 
-export const PxChartSharedConfigTabs = ({configLocal, setConfigLocal, closeModal, updating}: Props) => {
+export const PxChartSharedConfigTabs = ({closeModal}: Props) => {
   return (
     <Tab.Container defaultActiveKey="layout">
       <Row className="g-3 p-3">
@@ -31,11 +26,12 @@ export const PxChartSharedConfigTabs = ({configLocal, setConfigLocal, closeModal
             <Nav.Item>
               <Nav.Link eventKey="layout" className={styles['tab-group']}>版面配置</Nav.Link>
             </Nav.Item>
-            {Object.keys(groupedSharedConfigEntries).map((group) => (
-              <Nav.Item key={group}>
-                <Nav.Link eventKey={group} className={styles['tab-group']}>{group}</Nav.Link>
-              </Nav.Item>
-            ))}
+            {/* --- Disable market Px update / history Px request interval --- */}
+            {/* {Object.keys(groupedSharedConfigEntries).map((group) => (*/}
+            {/*  <Nav.Item key={group}>*/}
+            {/*    <Nav.Link eventKey={group} className={styles['tab-group']}>{group}</Nav.Link>*/}
+            {/*  </Nav.Item>*/}
+            {/* ))}*/}
           </Nav>
         </Col>
         <Col sm={9}>
@@ -43,42 +39,43 @@ export const PxChartSharedConfigTabs = ({configLocal, setConfigLocal, closeModal
             <Tab.Pane eventKey="layout">
               <ChartLayoutSelector onSelect={closeModal}/>
             </Tab.Pane>
-            {Object.entries(groupedSharedConfigEntries).map(([group, entries]) => (
-              <Tab.Pane key={group} eventKey={group}>
-                {Object.entries(entries).map(([key, entry]) => {
-                  const configKey = key as PxSharedConfigKeys;
-                  const {title, isDisabled, tips, min, step} = entry;
+            {/* --- Disable market Px update / history Px request interval --- */}
+            {/* {Object.entries(groupedSharedConfigEntries).map(([group, entries]) => (*/}
+            {/*  <Tab.Pane key={group} eventKey={group}>*/}
+            {/*    {Object.entries(entries).map(([key, entry]) => {*/}
+            {/*      const configKey = key as PxSharedConfigKeys;*/}
+            {/*      const {title, isDisabled, tips, min, step} = entry;*/}
 
-                  const value = getSharedConfig(configLocal, configKey);
-                  const disabled = updating || (isDisabled && isDisabled(configLocal));
+            {/*      const value = getSharedConfig(configLocal, configKey);*/}
+            {/*      const disabled = updating || (isDisabled && isDisabled(configLocal));*/}
 
-                  return (
-                    <FloatingInput
-                      key={key}
-                      type="number"
-                      label={title}
-                      value={value}
-                      onChange={({target}) => setConfigLocal({
-                        ...configLocal,
-                        [configKey]: (target.value === '' || target.value === '0') ?
-                          (min || defaultSharedConfig[configKey]) :
-                          parseFloat(target.value),
-                      })}
-                      className="w-100 mb-3"
-                      disabled={disabled}
-                      min={min}
-                      step={step}
-                      description={tips}
-                    />
-                  );
-                })}
-                <Row className="text-end">
-                  <Col>
-                    {updating && <TextWithLoading show={updating} text="更新中"/>}
-                  </Col>
-                </Row>
-              </Tab.Pane>
-            ))}
+            {/*      return (*/}
+            {/*        <FloatingInput*/}
+            {/*          key={key}*/}
+            {/*          type="number"*/}
+            {/*          label={title}*/}
+            {/*          value={value}*/}
+            {/*          onChange={({target}) => setConfigLocal({*/}
+            {/*            ...configLocal,*/}
+            {/*            [configKey]: (target.value === '' || target.value === '0') ?*/}
+            {/*              (min || defaultSharedConfig[configKey]) :*/}
+            {/*              parseFloat(target.value),*/}
+            {/*          })}*/}
+            {/*          className="w-100 mb-3"*/}
+            {/*          disabled={disabled}*/}
+            {/*          min={min}*/}
+            {/*          step={step}*/}
+            {/*          description={tips}*/}
+            {/*        />*/}
+            {/*      );*/}
+            {/*    })}*/}
+            {/*    <Row className="text-end">*/}
+            {/*      <Col>*/}
+            {/*        {updating && <TextWithLoading show={updating} text="更新中"/>}*/}
+            {/*      </Col>*/}
+            {/*    </Row>*/}
+            {/*  </Tab.Pane>*/}
+            {/* ))}*/}
           </Tab.Content>
         </Col>
       </Row>
